00033b9b64adc4223be3dcb40e0dbfda36e33c55
Banaple151 <jungsu@banaple.co.kr>
2026-03-17 16:46:38 +0900

    maestro 테스트 수정

--- S.Kiosk/Kiosk_s.bat
+++ S.Kiosk/Kiosk_s.bat
@@ -3,16 +3,16 @@ cd /d "%~dp0"
 
 echo ===== Kiosk Test Start =====
 
-for %%f in (*.yaml) do @echo Running %%f ... & @maestro test "%%f" --format junit & @if errorlevel 1 (
-    @echo RESULT: FAIL
-    @echo ===== Test Stopped Due To Failure =====
-    @pause
-    @exit /b 1
-) else (
-    @echo RESULT: PASS
-    @echo.
+for %%f in (*.yaml) do (
+    echo Running %%f ...
+    call maestro test "%%f" --format junit
+    if errorlevel 1 (
+        echo RESULT: FAIL
+        goto END
+    )
+    echo RESULT: PASS
+    echo.
 )
 
-echo ===== All Tests Finished =====
-pause
-exit /b 0
+:END
+cmd /k
